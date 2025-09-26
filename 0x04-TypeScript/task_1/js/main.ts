2c2a824ecc6b89c856c4f980dc90a633a073c317
BetNoelM <noelm@betsoftware.com>
2025-09-26 16:52:58 +0200

    Update main.ts

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,6 +1,6 @@
 export interface Teacher {
     readonly firstName: string;
-   readonly lastName: string;
+    readonly lastName: string;
     fullTimeEmployee: boolean;
     yearsOfExperience?: number;
     location: string;
